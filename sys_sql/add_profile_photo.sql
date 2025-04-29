6770e7eb0aabe1f80804f219ab07b9b1aae0bc42
Guido Ruiz <ruizbenitezguido11@gmail.com>
2025-04-29 08:12:36 -0300

    actualizado 7.1

--- sys_sql/add_profile_photo.sql
+++
@@ -1,6 +0,0 @@
--- Agregar columna profile_photo a la tabla rh_person
-ALTER TABLE public.rh_person
-ADD COLUMN profile_photo VARCHAR(255) NULL;
-
--- Comentario para la columna
-COMMENT ON COLUMN public.rh_person.profile_photo IS 'Ruta de la imagen de perfil del usuario';
